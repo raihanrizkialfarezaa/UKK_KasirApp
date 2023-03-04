de551503470b9a83281046223b8ea3dcb9d304d8
RaihanRizkiAlfareza_29_XRPL2 <73086488+raihanrizkialfareza29XR2@users.noreply.github.com>
2023-03-04 14:44:01 +0700

    update_fe

--- resources/views/page/includes/footer.blade.php
+++ resources/views/page/includes/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                2020 © RizkiGroups.
+                2022 © Raihan Rizki Alfareza
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-right d-none d-sm-block">
-                    Design & Develop by Iqbal
+                    Design & Develop by Raihan Rizki Alfareza
                 </div>
             </div>
         </div>
